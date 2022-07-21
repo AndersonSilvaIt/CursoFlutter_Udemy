b09f1a6de2d507f05758752b8d65837f9fbf6646
a.silva@gestaoagropecuaria.com.br <a.silva@gestaoagropecuaria.com.br>
2022-07-21 15:45:04 -0300

    Criado botões no App

--- Projetos/projeto_perguntas/lib/main.dart
+++ Projetos/projeto_perguntas/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -7,15 +9,41 @@ void main(List<String> args) {
 
 class PerguntaApp extends StatelessWidget {
   
+void responder(){
+  print('Pergunta Respondida');
+}
+
+
 @override
   Widget build(BuildContext context) {
-    
+
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita',
+      'Qual é o seu animal favorito'
+    ];
+
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text('Perguntas'),
           ),
-          body: Text('Olá flutter !!!'),
+          body: Column(
+            children: [
+              Text(perguntas[0]),
+              RaisedButton(
+                child: Text('Resposta 1'),
+                onPressed: responder,
+              ),
+              RaisedButton(
+                child: Text('Resposta 2'),
+                onPressed:   () { print('Resposta 2 foi selecionada');},
+              ),
+              RaisedButton(
+                child: Text('Resposta 3'),
+                onPressed: () => print('Resposta 3'),
+              ),                            
+            ],
+          ),
       ),
     );
   }
