0ba78506da69809ae0dbd270d3ea70f09e8ad6af
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-09-08 13:06:28 -0300

    Implementado os componentes iniciais

--- Projetos/meals/lib/screeens/categories_screen.dart
+++ Projetos/meals/lib/screeens/categories_screen.dart
@@ -1,20 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:meals/components/categorya_item.dart';
+import 'package:meals/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
-    return GridView(
-
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        mainAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos Cozinhar?'),
       ),
-      children: [],
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategories.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
