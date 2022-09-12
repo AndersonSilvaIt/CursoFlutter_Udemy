60b9e8081e4aec75749ad7c78f30eb63b6a7a55d
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-09-12 17:57:59 -0300

    Implementado tela de configurações

---
+++ Projetos/meals/lib/models/settings.dart
@@ -0,0 +1,15 @@
+class Settings {
+  late bool isGlutenFree;
+  late bool isLactoseFree;
+  late bool isVegan;
+  late bool isVegetarian;
+
+  Settings({
+    this.isGlutenFree = false,
+    this.isLactoseFree = false,
+    this.isVegan = false,
+    this.isVegetarian = false,
+  });
+
+  
+}
