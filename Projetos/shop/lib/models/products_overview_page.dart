e5f4c7e77507dc74261f10c661cf7f7c68c2a243
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-09-12 22:50:40 -0300

    Criado a grid inicial do projeto

--- Projetos/shop/lib/models/products_overview_page.dart
+++ Projetos/shop/lib/models/products_overview_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
-
-import '../data/dummy_data.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   final List<Product> loadedProducts = dummyProducts;
@@ -14,17 +14,15 @@ class ProductsOverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
       ),
     );
