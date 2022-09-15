4c9456dffdce6da0ec6102518d33c96f7601efd2
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-09-15 17:26:49 -0300

    Mudando o estado de um componente específico na árvore

--- Projetos/shop/lib/components/product_item.dart
+++ Projetos/shop/lib/components/product_item.dart
@@ -10,7 +10,10 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      listen: false,
+    );
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -28,13 +31,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-              onPressed: () {
-                product.toggleFavorite();
-              },
-              icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
-              color: Theme.of(context).colorScheme.secondary),
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+                onPressed: () {
+                  product.toggleFavorite();
+                },
+                icon: Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                color: Theme.of(context).colorScheme.secondary),
+          ),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
