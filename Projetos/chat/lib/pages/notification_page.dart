322bd53e63eced5d7745ccac3f13d6901312617c
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-10-25 09:53:28 -0300

    Implementado tela de notificações

--- Projetos/chat/lib/pages/notification_page.dart
+++ Projetos/chat/lib/pages/notification_page.dart
@@ -1,17 +1,27 @@
+import 'package:chat/core/services/notification/push_notification_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final service = Provider.of<ChatNotificationService>(context);
+
+    final items = service.items;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Minhas Notificações'),
-      ),
-      body: const Center(
-        child: Text('Notificações'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Minhas Notificações'),
+        ),
+        body: ListView.builder(
+          itemCount: service.itemsCont,
+          itemBuilder: (ctx, i) => ListTile(
+            title: Text(items[i].title),
+            subtitle: Text(items[i].body),
+            onTap: () => service.remove(i),
+          ),
+        ));
   }
 }
