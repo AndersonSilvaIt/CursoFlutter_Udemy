88e2cfba6bb4805a57fa86eecf94f8c349e714fb
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-10-25 09:36:53 -0300

    Implementado icone de notificações

---
+++ Projetos/chat/lib/core/models/chat_notification.dart
@@ -0,0 +1,11 @@
+class ChatNotification {
+  final String title;
+  final String body;
+
+  ChatNotification({
+    required this.title,
+    required this.body,
+  });
+
+  
+}
--- Projetos/chat/lib/pages/chat_page.dart
+++ Projetos/chat/lib/pages/chat_page.dart
@@ -1,7 +1,11 @@
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_message.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../core/services/notification/push_notification_service.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -12,33 +16,65 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('DBR Chat'),
         actions: [
-          DropdownButton(
-            icon: const Icon(
-              Icons.more_vert,
-              color: Colors.blue,
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.blue,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logou',
+                  child: Container(
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.black87,
+                        ),
+                        SizedBox(width: 10),
+                        Text('Sair')
+                      ],
+                    ),
+                  ),
+                )
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
+              },
             ),
-            items: [
-              DropdownMenuItem(
-                value: 'logou',
-                child: Container(
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Colors.black87,
-                      ),
-                      SizedBox(width: 10),
-                      Text('Sair')
-                    ],
+          ),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (ctx) {
+                        return const NotificationPage();
+                      },
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.notifications),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    '${Provider.of<ChatNotificationService>(context).itemsCont}',
+                    style: const TextStyle(
+                      fontSize: 12,
+                    ),
                   ),
                 ),
-              )
+              ),
             ],
-            onChanged: (value) {
-              if (value == 'logout') {
-                AuthService().logout();
-              }
-            },
           )
         ],
       ),
