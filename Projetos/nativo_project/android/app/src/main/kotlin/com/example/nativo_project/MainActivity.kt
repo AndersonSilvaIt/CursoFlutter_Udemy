ac97b05b0ce7c58767e58cce1c9cfb2b4415df11
Anderson Silva <a.silva@gestaoagropecuaria.com.br>
2022-10-31 20:19:20 -0300

    Implementado integração com o IOS

--- Projetos/nativo_project/android/app/src/main/kotlin/com/example/nativo_project/MainActivity.kt
+++ Projetos/nativo_project/android/app/src/main/kotlin/com/example/nativo_project/MainActivity.kt
@@ -1,6 +1,27 @@
 package com.example.nativo_project
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "dbrcode.com.br/nativo"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine){
+        super.configureFlutterEngine(FlutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result -> 
+
+            if(call.method == "calcSum"){
+                val a = call.argument<Int>("a")?.toInt() ?: 0
+                val b = call.argument<Int>("b")?.toInt() ?: 0
+                result.success(a+b)
+            }else{
+                result.notImplemented()
+            }
+        }
+
+    }
+
 }
